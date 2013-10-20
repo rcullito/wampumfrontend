6423e89f42d15b0a6f68885cef1135a005a4740b
Rob Culliton <rculliton@yieldbot.com>
2013-10-20 18:09:58 -0400

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wampumfrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
