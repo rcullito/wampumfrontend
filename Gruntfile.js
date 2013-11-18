003ca853806f8eca391792716311f48af6fed2b1
Rob Culliton <rculliton@yieldbot.com>
2013-11-17 21:51:03 -0500

    use our very own git clean module to get ride of the extra docs!!

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ var mountFolder = function (connect, dir) {
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-git-clean');
 
   // configurable paths
   var yeomanConfig = {
