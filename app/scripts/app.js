02edd94fe3ebb9a393314466af2632e1ddd41d02
Rob Culliton <rculliton@yieldbot.com>
2013-11-17 21:45:48 -0500

    update routes to be served out of wampumbackend, bump angular version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
-angular.module('wampumfrontendApp', [])
-  .config(function ($routeProvider) {
+angular.module('wampumfrontendApp', ['ngRoute'])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
+      .when('/ui/wampumfrontend', {
+        templateUrl: '/ui/wampumfrontend/app/views/main.html',
         controller: 'MainCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/ui'
       });
   });
