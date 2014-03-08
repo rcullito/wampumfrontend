d558150fdfe5d1197cc06ae83c6d2e368e82056b
Rob Culliton <rculliton@yieldbot.com>
2014-03-08 14:57:04 -0500

    get disqus working, start pullign out event code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,18 +3,6 @@
 angular.module('wampumfrontendApp')
   .controller('MainCtrl', function ($scope, $sce, $window, $routeParams, $location, esService, blogService, disqusService) {
 
-    var raw_event = {workflow: 'pageload'};
-
-    esService.eventCollector(raw_event, function (err, status) {
-      if (err) {
-        console.log('something went wrong');
-        console.log(err);
-      } else {
-        console.log('event collected');
-        console.log(status);
-      }
-    });
-
     var path = $location.path().split('/');
     var suburl = _.last(path);
     // console.log(suburl);
@@ -35,40 +23,18 @@ angular.module('wampumfrontendApp')
 
     if (suburl === 'blog') {
       $scope.suburl = 'blog';
+      disqusService.loadDisqus();
     } else if (!suburl || suburl === 'about') {
       $scope.suburl = 'about';
     }
 
 
-    // disqusService.loadDisqus();
-
-
-
-
-
+    
   	
   	$scope.search = function(term) {
-      // find a better way to update the url without reloading the page
-      // $location.path('/search/' + term);
-
       // fire off an async request to the raw events here that the user has searched for something
       // time stamp,
       // location
-      var raw_event = {
-        search_term: term,
-        referrer_url: document.referrer,
-        workflow: 'searching',
-      };
-
-      esService.eventCollector(raw_event)
-        .success(function(data) {
-          console.log('event collected!')
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log('something went wrong');
-          console.log(data);
-        });
 
       esService.prefixQuery('organizations', term)
         .success(function(data) {
