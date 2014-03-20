e8255c857eccec90bfc0f34df9a270b014f50382
Rob Culliton <rculliton@yieldbot.com>
2014-03-19 21:32:41 -0400

    experimenting with animation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,6 +31,8 @@ angular.module('wampumfrontendApp')
     // $scope.$watch('suburl', function(newVal, oldVal) {
     //   console.log(newVal);
     // });
+
+
       var stage = new Kinetic.Stage({
         container: 'kinetic',
         width: 800,
@@ -53,72 +55,123 @@ angular.module('wampumfrontendApp')
         closed: true
       });
 
-      var group = new Kinetic.Group({
-        x: 20,
-        y: 60
-      });
+      // var group = new Kinetic.Group({
+      //   x: 20,
+      //   y: 60
+      // });
 
-      var frameLine1 = new Kinetic.Line({
-        points: [50, 50, 35, 35],
-        stroke: 'black',
-        strokeWidth: 2,
-        lineCap: 'round',
-        lineJoin: 'round'
-      });
+      // var frameLine1 = new Kinetic.Line({
+      //   points: [50, 50, 35, 35],
+      //   stroke: 'black',
+      //   strokeWidth: 2,
+      //   lineCap: 'round',
+      //   lineJoin: 'round'
+      // });
 
-      var frameLine2 = new Kinetic.Line({
-        points: [70, 50, 55, 35],
-        stroke: 'black',
-        strokeWidth: 2,
-        lineCap: 'round',
-        lineJoin: 'round'
-      });
+      // var frameLine2 = new Kinetic.Line({
+      //   points: [70, 50, 55, 35],
+      //   stroke: 'black',
+      //   strokeWidth: 2,
+      //   lineCap: 'round',
+      //   lineJoin: 'round'
+      // });
 
-      var circle1 = new Kinetic.Circle({
-        x: 50,
-        y: 50,
-        radius: 8,
-        fill: 'black'
-      });
+      // var circle1 = new Kinetic.Circle({
+      //   x: 50,
+      //   y: 50,
+      //   radius: 8,
+      //   fill: 'black'
+      // });
 
-      var circle2 = new Kinetic.Circle({
-        x: 65,
-        y: 50,
-        radius: 8,
-        fill: 'black'
-      });
+      // var circle2 = new Kinetic.Circle({
+      //   x: 65,
+      //   y: 50,
+      //   radius: 8,
+      //   fill: 'black'
+      // });
 
-      group.add(circle1);
-      group.add(circle2);
-      group.add(frameLine1);
-      group.add(frameLine2);
+      // group.add(circle1);
+      // group.add(circle2);
+      // group.add(frameLine1);
+      // group.add(frameLine2);
 
       layer.add(jeans);
-      layer.add(group);
+      // layer.add(group);
       stage.add(layer);
 
-      var tween1 = new Kinetic.Tween({
-        node: jeans,
-        duration: 6,
-        x: 700,
-        y: 30,
-        rotation: 10
-      });
 
-      var tween2 = new Kinetic.Tween({
-        node: group,
-        duration: 6,
-        x: 700,
-        y: 20
-      });
 
-      setTimeout(function() {
-        tween1.play();
-      }, 1000);
+      // // in ms
 
-      setTimeout(function() {
-        tween2.play();
-      }, 1000);
+      var anim = new Kinetic.Animation(function(frame) {
+        jeans.setX(150 * Math.sin(frame.time * 2 * Math.PI / 2000) + 200);
+
+
+        jeans.setX(150 * frame.time / 2000 + 200);
+
+        // come up with a lodash thing in between zero and 1 here
+
+        // console.log(Math.sin(frame.time * 2 * Math.PI / 2000));
+
+        // console.log(frame.time);
+        // console.log(amplitude * Math.sin(frame.time * 2 * Math.PI / period) + 200);
+
+
+        // jeans.setX(frame.time / period);
+      }, layer);
+
+      anim.start();
+
+      // var tween1 = new Kinetic.Tween({
+      //   node: jeans,
+      //   duration: 6,
+      //   x: 700,
+      //   y: 30,
+      //   rotation: 10
+      // });
+
+      // var tween2 = new Kinetic.Tween({
+      //   node: group,
+      //   duration: 6,
+      //   x: 700,
+      //   y: 20
+      // });
+
+      // tween1.play();
+      // tween2.play();
+
+      //  var stage = new Kinetic.Stage({
+      //   container: 'kinetic',
+      //   width: 578,
+      //   height: 200
+      // });
+      // var layer = new Kinetic.Layer();
+
+      // var hexagon = new Kinetic.RegularPolygon({
+      //   x: stage.width()/2,
+      //   y: stage.height()/2,
+      //   sides: 6,
+      //   radius: 70,
+      //   fill: 'red',
+      //   stroke: 'black',
+      //   strokeWidth: 4
+      // });
+
+      // layer.add(hexagon);
+      // stage.add(layer);
+
+      // var amplitude = 150;
+      // var period = 2000;
+      // // in ms
+      // var centerX = stage.width()/2;
+
+      // var anim = new Kinetic.Animation(function(frame) {
+      //   hexagon.setX(amplitude * Math.sin(frame.time * 2 * Math.PI / period) + centerX);
+      // }, layer);
+
+      // anim.start();
+
+
 
 
     // Search
