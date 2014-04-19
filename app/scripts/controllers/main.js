d849b86bd72cf2582c09b8c1510181ad92b350b6
Rob Culliton <rculliton@yieldbot.com>
2014-04-19 04:58:30 -0400

    prefetch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -44,39 +44,29 @@ angular.module('wampumfrontendApp')
 
     // BEGINNING OF TYPEAHEAD STUFF
  
-      var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-      ];
-
-      // constructs the suggestion engine
-      var states = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      var countries = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        // `states` is an array of state names defined in "The Basics"
-        local: $.map(states, function(state) { return { value: state }; })
+        limit: 10,
+        prefetch: {
+          url: '/ui/wampumfrontend/app/data/countries1.json',
+          filter: function(list) {
+            return $.map(list, function(country) { return { name: country }; });
+          }
+        }
       });
-       
-      // kicks off the loading/processing of `local` and `prefetch`
-      states.initialize();
 
-      $('#bloodhound .typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-      },
-      {
-        name: 'states',
-        displayKey: 'value',
+      // kicks off the loading/processing of `local` and `prefetch`
+      countries.initialize();
+
+      // passing in `null` for the `options` arguments will result in the default
+      // options being used
+      $('#prefetch .typeahead').typeahead(null, {
+        name: 'countries',
+        displayKey: 'name',
         // `ttAdapter` wraps the suggestion engine in an adapter that
         // is compatible with the typeahead jQuery plugin
-        source: states.ttAdapter()
+        source: countries.ttAdapter()
       });
 
 
