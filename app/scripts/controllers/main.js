3a3a659dc4efe2c709375a9eee42a2cfa0393449
Rob Culliton <rculliton@yieldbot.com>
2014-06-13 10:41:20 -0400

    move uuid function into its own service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,7 @@
 'use strict';
 
 angular.module('wampumfrontendApp')
-  .controller('MainCtrl', ['$scope', '$routeParams', '$location', 'esService', function ($scope, $routeParams, $location, esService) {
-
-    var createUUID = function() {
-      // http://www.ietf.org/rfc/rfc4122.txt
-      var s = [];
-      var hexDigits = "0123456789abcdef";
-      for (var i = 0; i < 36; i++) {
-          s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
-      }
-      s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
-      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
-      s[8] = s[13] = s[18] = s[23] = "-";
-
-      var uuid = s.join("");
-      return uuid;
-    };   
-
+  .controller('MainCtrl', ['$scope', '$routeParams', '$location', 'esService', 'uuidService', function ($scope, $routeParams, $location, esService, uuidService) {
 
     $scope.yetToSignUp = true; 
 
@@ -59,7 +43,7 @@ angular.module('wampumfrontendApp')
         var term = $('#searchinput').val();
       }
 
-      $scope.requestid = createUUID();
+      $scope.requestid = uuidService.createUUID();
       
       $location.search('search', term);
       $scope.term = term;
