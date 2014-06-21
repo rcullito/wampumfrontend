202085aed8da483fdde29aba2b101eb89566db9b
Rob Culliton <rculliton@yieldbot.com>
2014-06-21 19:07:09 -0400

    notes on the current state of play

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,6 +3,16 @@
 angular.module('wampumfrontendApp')
   .controller('LoginCtrl', ['$scope', '$routeParams', '$location', 'authService', function ($scope, $routeParams, $location, authService) {
 
+    // users are brought here from the sign up button and also
+    // from the free shipping button
+    // so the first thing we need to know is if they are already logged
+    // in
+
+    // if they are then they should have passed along query params
+
+    // if they are refreshing this page they will have come from express
+    // rather than angular routing though
+
     $scope.register = function (email, password) {
       authService.register(email, password)
         .success(function (data) {
