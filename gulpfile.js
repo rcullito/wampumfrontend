b801c646bb32a1716d9ad5f96a996400bf436c90
Rob Culliton <rculliton@yieldbot.com>
2014-06-28 08:48:09 -0400

    about halfway to recreating build with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,37 @@ var concat = require('gulp-concat');
 
 
 
-gulp.task('build', function() {
+gulp.task('concatlocaljs', function() {
   return gulp.src('app/scripts/**/*.js')
     .pipe(concat('wampum.js'))
+    .pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('concatvendorjs', function () {
+  return gulp.src('app/public/typeaheadFork.min.js')
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('copyindexfile', function () {
+  return gulp.src('app/index.html')
     .pipe(gulp.dest('dest'));
 });
 
-
-gulp.task('default', function () {
-  console.log('welcome');
+gulp.task('copyviews', function () {
+  return gulp.src('app/views/*.html')
+    .pipe(gulp.dest('dest'));
 });
+
+// get dest to resemble dist exactly and then cut everything over
+
+// worry about watch second
+
+
+gulp.task('default', [
+  'concatlocaljs',
+  'concatvendorjs',
+  'copyindexfile',
+  'copyviews'
+]);
+
