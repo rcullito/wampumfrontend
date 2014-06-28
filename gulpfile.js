e3881066571c9f1f059d0789ed01879959346bb8
Rob Culliton <rculliton@yieldbot.com>
2014-06-28 09:24:46 -0400

    progressing on gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,46 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+  cssmin = require('gulp-cssmin'),
+  concat = require('gulp-concat');
 
 
 
-gulp.task('concatlocaljs', function() {
+gulp.task('localjs', function() {
   return gulp.src('app/scripts/**/*.js')
     .pipe(concat('wampum.js'))
     .pipe(gulp.dest('dest/js'));
 });
 
-gulp.task('concatvendorjs', function () {
+gulp.task('vendorjs', function () {
   return gulp.src('app/public/typeaheadFork.min.js')
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('dest/js'));
 });
 
-gulp.task('copyindexfile', function () {
+gulp.task('index', function () {
   return gulp.src('app/index.html')
     .pipe(gulp.dest('dest'));
 });
 
-gulp.task('copyviews', function () {
+gulp.task('views', function () {
   return gulp.src('app/views/*.html')
     .pipe(gulp.dest('dest'));
 });
 
-// get dest to resemble dist exactly and then cut everything over
+gulp.task('css', function () {
+  return gulp.src(['app/styles/typeahead.css', 'app/public/css/buttons.css', 'app/styles/bootstrap.css', 'app/styles/main.css'])
+    .pipe(concat('all.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('dest'))
+})
 
 // worry about watch second
 
 
 gulp.task('default', [
-  'concatlocaljs',
-  'concatvendorjs',
-  'copyindexfile',
-  'copyviews'
+  'localjs',
+  'vendorjs',
+  'index',
+  'views',
+  'css'
 ]);
 
