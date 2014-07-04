682b8cdf2f4caad666ddce4563a4c9f6c6fae20a
Rob Culliton <rculliton@yieldbot.com>
2014-07-04 14:23:20 -0400

    transclue through

--- app/directives/directive.js
+++ app/directives/directive.js
@@ -3,6 +3,7 @@ angular.module("wampumfrontendApp")
 .directive("superman", function() {
     return {
         restrict: "E",
+        transclude: true,
         templateUrl: "/ui/wampumfrontend/dest/header.html"
     }
 })
