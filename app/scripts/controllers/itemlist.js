58bbb71e30a1f391bf4948d718a9f91e4081c26d
Rob Culliton <rculliton@yieldbot.com>
2014-07-04 17:03:44 -0400

    list to search is working great

--- app/scripts/controllers/itemlist.js
+++ app/scripts/controllers/itemlist.js
@@ -7,7 +7,7 @@ angular.module('wampumfrontendApp')
 
     itemListService.tags()
       .success(function (data) {
-        $scope.tags = _.chain(data)
+        var tags = _.chain(data)
           .groupBy(function (entry) {
             return _.first(entry.tag);
           })
@@ -21,6 +21,14 @@ angular.module('wampumfrontendApp')
             }
           })
           .value();
+
+
+      var about_a_third = Math.floor(tags.length / 3);
+      var two_thirds = about_a_third * 2;
+
+      $scope.firsttags = _.at(tags, _.range(0, about_a_third));
+      $scope.secondtags = _.at(tags, _.range(about_a_third, two_thirds));
+      $scope.thirdtags = _.at(tags, _.range(two_thirds, tags.length));
 
       })
       .error(function (err) {
