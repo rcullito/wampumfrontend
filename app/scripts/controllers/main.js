13aa75a3c92eb13382cf137e1508cee4b1599d3f
Rob Culliton <rculliton@yieldbot.com>
2014-08-06 19:40:56 -0400

    some good cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,94 +3,10 @@
 angular.module('wampumfrontendApp')
   .controller('MainCtrl', ['$scope', '$routeParams', '$location', '$cookies', 'esService', function ($scope, $routeParams, $location, $cookies, esService) {
 
-
-    $scope.showAbout = function () {
-      $scope.motion = true;
-      $scope.resultObjects = null;
-      $location.url($location.path());
+    $scope.originofWampum = function () {
+      alert('Wampum is a Native American word for shell beads that were used as a form of currency.');
     };
 
-
-    $scope.motion = true;
-    $('#searchinput').val();
-    
-    $scope.search = function(term) {
-      $scope.motion = false;
-      $location.search('search', term);
-      $scope.term = term;
-
-      esService.search(term)
-        .success(function (results) {
-          if (!_.isEmpty(results)) {
-            $scope.resultObjects = results;
-            $scope.noResults = false;
-          } else {
-            $scope.resultObjects = false;
-            $scope.noResults = true;
-          }
-          $location.search('search', term);
-        })
-        .error(function (err) {
-          console.log(err);
-        });
-    };
-
-    var urlSearchParams = $location.search();
-
-    if (urlSearchParams.search) {
-      $scope.search(urlSearchParams.search);
-    };    
-
-    // BEGINNING OF TYPEAHEAD STUFF
-
-    $('#scrollable-dropdown-menu .typeahead').on("typeahead:opened", function () {
-      $('#rc-dropdown').css('width', searchinputWidth);
-    });
-
-
-    var searchinputWidth = $('#searchinput').css('width');
-
-    var engine = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('tag'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      limit: 10,
-      prefetch: {
-        url: '/tags',
-        filter: function(res) {
-          return res;
-        }
-      }
-    });
-
-    engine.initialize();
-
-    $('#scrollable-dropdown-menu .typeahead').typeahead({
-      minLength: 1,
-      highlight: false,
-    }, {
-      name: 'engine',
-      displayKey: 'tag',
-      source: engine.ttAdapter()
-    });
-
-    $('#searchinput').keyup(function (key) {
-      if (key.which === 13) {
-        $('.typeahead').typeahead('close');
-        $scope.search($('#searchinput').val());
-        return;
-      }
-      var current_term = $('#searchinput').val();
-      engine.get(current_term, function (suggestions) {
-        var top_hit = _.first(suggestions);
-        if (top_hit) {
-         var search_term = top_hit.tag;
-          $scope.search(search_term);         
-        }
-      });
-    });
-
-    $('#gobutton').click(function () {
-      $scope.search($('#searchinput').val());
-    });
+    $scope.brands = 'jcrew';
 
 }]);
