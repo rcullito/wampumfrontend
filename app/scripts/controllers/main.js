49502bbd56129df6a58753795af0bfcdea98f6c5
Rob Culliton <rculliton@yieldbot.com>
2014-08-06 22:55:04 -0400

    w00t

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,15 @@
 angular.module('wampumfrontendApp')
   .controller('MainCtrl', ['$scope', '$routeParams', '$location', '$cookies', 'esService', function ($scope, $routeParams, $location, $cookies, esService) {
 
+    $scope.initialform = true;
+
     $scope.originofWampum = function () {
       alert('Wampum is a Native American word for shell beads that were used as a form of currency.');
     };
 
-    $scope.initialform = true;
+    $scope.showAbout = function () {
+      $scope.initialform = true;
+    };
 
     $scope.go = function () {
       $scope.initialform = false;
