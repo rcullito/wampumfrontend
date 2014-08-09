e2c82166e43820781adda17de778b4c6f8c47773
Rob Culliton <rculliton@yieldbot.com>
2014-08-09 18:40:38 -0400

    ui cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,6 @@
 angular.module('wampumfrontendApp')
   .controller('MainCtrl', ['$scope', '$routeParams', '$location', '$cookies', 'mainService', function ($scope, $routeParams, $location, $cookies, mainService) {
 
-    $scope.initialform = true;
-    $scope.submitted = false;
-
     smoothScroll.init();
 
     $scope.clothingtypes = 'shirts';
