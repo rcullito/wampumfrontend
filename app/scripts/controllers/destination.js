f89a3d0d6dd1d52e021821ef8ed2651145628113
Rob Culliton <rculliton@yieldbot.com>
2014-09-14 10:31:29 -0400

    have wampum warehouse come to the rescue when we dont have any results for a given clothing type

--- app/scripts/controllers/destination.js
+++ app/scripts/controllers/destination.js
@@ -9,7 +9,7 @@ angular.module('wampumfrontendApp')
 
     $scope.secondform = true;
 
-    var getRandomEntry = function (collection) {
+    var getResultInfo = function (collection) {
       var collection_length = collection.length;
       var random_key = _.random(0, collection_length - 1);
       return  {
@@ -22,8 +22,21 @@ angular.module('wampumfrontendApp')
       mainService.clothingSearch($scope.clothingtype)
         .success(function (data) {
           // TODO figure something out for when we don't have any resutls for the other type
-          $scope.entry = data[getRandomEntry(data).random_key];
-          $scope.locationid = $scope.entry.id;
+          var resultInfo = getResultInfo(data);
+          var resultLength = resultInfo.collection_length;
+          if (resultLength) {
+            console.log('we have results');
+            var resultKey = resultInfo.random_key;
+            $scope.entry = data[resultKey];
+            $scope.result_length = resultInfo.collection_length;
+          } else {
+            $scope.result_length = 1;
+            $scope.entry = {
+              name: 'Wampum Warehouse',
+              description: "Wampum Warehouse is where we accept items that are currently not accepted by any of our partners. We'll accept any type of clothing!",
+              id: 0
+            };
+          }
         })
         .error(function (err) {
           console.log(err);
